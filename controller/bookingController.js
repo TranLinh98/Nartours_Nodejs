206fbef771e2a74d8f1a6e92faa60a012dfe1836
linhvinh <70695174+linhvinh@users.noreply.github.com>
2020-12-27 21:38:31 +0700

    Booking stripe

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -8,7 +8,7 @@ const factory = require('./handlerFactory');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  //console.log(tour);
+  // console.log(tour);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
@@ -40,16 +40,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session
   });
 });
-
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
